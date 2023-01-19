e289d5c0a26fda8914885b0393b00e5b0964376a
SEHEE PARK <sehii0327@gmail.com>
2023-01-19 15:27:48 +0900

    Express 초기 환경 세팅 수정사항 완료

--- sehee/app.js
+++ sehee/app.js
@@ -1,28 +1,27 @@
-const http = require("http");
+require('dotenv').config();
+
+// const http = require("http")
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const dotenv = require("dotenv");
-
-dotenv.config()
-
 const { DataSource } = require('typeorm')
-const myDataSource = new DataSource ({
-type: process.env.TYPEORM_CONNECTION,
-host: process.env.TYPEORM_HOST,
-port: process.env.TYPEORM_PORT,
-username: process.env.TYPEORM_USERNAME,
-password: process.env.TYPEORM_PASSWORD,
-database: process.env.TYPEORM_DATABASE
+
+const appDataSource = new DataSource ({
+type: process.env.DB_CONNECTION,
+host: process.env.DB_HOST,
+port: process.env.DB_PORT,
+username: process.env.DB_USERNAME,
+password: process.env.DB_PASSWORD,
+database: process.env.DB_DATABASE
 })
 
-myDataSource.initialize()
+appDataSource.initialize()
 .then( ()=> {
   console.log("Data Source has been initialized!")
 })
 .catch((err) => {
   console.error("Error during Data Source initialization", err)
-  myDataSource.destroy()
+  appDataSource.destroy()
 })
 
 
@@ -37,12 +36,12 @@ app.get("/ping", (req, res) => {
   res.status(200).json({ message : "pong"})
 });
 
-const server = http.createServer(app)
+// const server = http.createServer(app)
 const PORT = process.env.PORT;
 
 const start = async () => {
   try {
-  server.listen(PORT, () => console.log(`server is listening on ${PORT}`));
+  app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
 } catch (err) {
   console.error(err);
 }
