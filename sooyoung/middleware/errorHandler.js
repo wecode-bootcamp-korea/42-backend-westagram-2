4c862bd2295ef77fea5b587ddd7211b3b879ce82
sooyoung <fromsy2018@gmail.com>
2023-02-03 15:38:24 +0900

    [FIX] 에러 처리 위치 변경 - checkError에서 글로벌 에러 핸들러 함수로 변경

--- sooyoung/middleware/errorHandler.js
+++ sooyoung/middleware/errorHandler.js
@@ -2,15 +2,19 @@ const checkError = (controller) => {
   return async (req, res, next) => {
     try {
       await controller(req, res);
-      next();
     } catch (err) {
-      console.log('----에러 핸들러 작동----');
-      console.error(err);
-      res.status(err.statusCode || 500).json({ message: err.message });
+      next(err);
     }
   };
 };
 
+const globalErrorHandler = (err, req, res, next) => {
+  console.log('----에러 핸들러 작동----');
+  console.error(err);
+  res.status(err.statusCode || 500).json({ message: err.message });
+};
+
 module.exports = {
   checkError,
+  globalErrorHandler,
 };
